23087d18fb5434661e8bc559ecc33e5c4b7852ff
Shalini Tiwari <70230806+shalini-tiwari@users.noreply.github.com>
2023-07-18 17:50:36 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,6 @@
-//Slick Carousel Controllers
-$(".testimonial-reel").slick({
-  centerMode: true,
-  centerPadding: "40px",
-  dots: true,
-  slidesToShow: 3,
-  infinite: true,
-  arrows: false,
-  lazyLoad: "ondemand",
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        centerMode: false,
-      },
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
+const button = document.querySelector("#menu-button");
+const menu = document.querySelector("#menu");
+
+button.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
